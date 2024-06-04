c269a9d3a97d2da55aed8c75124a18c00db6c423
come-senusi-wale <akinyemisaheedwale@gmailcom>
2024-06-04 11:56:23 +0100

    ok

--- app/api/service.ts
+++ app/api/service.ts
@@ -33,3 +33,58 @@ export const unsubscribeToServiceModerator = (data: any) => {
 export const updateServiceHandleReq = (data: any) => {
     return axiosInstance().post("/user/worker/raider/service/updatehandle", data);
 }
+
+
+//// chatter ///////
+
+// chatter subscrption service
+export const subscriptionToService = (data: any) => {
+    return axiosInstance().post("/user/worker/chatter/service/subscribe", data);
+}
+
+export const getChatterServices = () => {
+    return axiosInstance().get("/user/worker/chatter/service/all?page=1&limit=50");
+}
+
+export const updateChatterServiceHandleReq = (data: any) => {
+    return axiosInstance().post("/user/worker/chatter/service/updatehandle", data);
+}
+
+
+// chatter task
+export const getChatterTask = () => {
+    return axiosInstance().get("/user/worker/chatter/task/other?limit=50&page=1");
+}
+
+export const getSingleChatterTask = (id: any) => {
+    return axiosInstance().get(`/user/worker/chatter/task/single_task/${id}`);
+}
+
+export const availbleChatterTask = (limit: number, page: number, status: string) => {
+    return axiosInstance().get(`/user/worker/chatter/task/avaibleTask/task?limit=${limit}&page=${page}&status=${status}`);
+}
+
+
+// work
+
+export const startChatTask = (body: any) => {
+    return axiosInstance().post("/user/worker/chatter/work/start_raid", body);
+}
+
+export const getStatusTask = (status: string) => {
+    return axiosInstance().get(`/user/worker/chatter/work/status/task?limit=80&page=1&status=${status}`);
+}
+
+export const getSinglechat = (chatId: string, status: string) => {
+    return axiosInstance().get(`/user/worker/chatter/work/single/task?chatId=${chatId}&status=${status}`);
+}
+
+export const getTotalStatusTask = (status: string) => {
+    return axiosInstance().get(`/user/worker/chatter/work/status/total?status=${status}`);
+}
+
+export const completeChatTask = (body: any) => {
+    return axiosInstance().post("/user/worker/chatter/work/complete_raid", body);
+}
+
+
