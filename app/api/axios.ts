924b5324a965da19b77cad45c4cf3cddee5a6d10
jude <justclassic24@gmail.com>
2024-10-18 21:55:23 +0100

    fixed deployment

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 export const axiosInstance = () => {
     const token = typeof window !== "undefined" && localStorage.getItem("bmdao-token");
     return axios.create({
-        baseURL: process.env.BACKEND_HOST,
+        baseURL: 'https://api.bmdao.xyz/api',
         headers: {
             Authorization: !!token ? `Bearer ${token}` : "",
         }
